9c8a64e612a28acc616cb002c07f40610ab06d56
wangyujiang <1451234775@qq.com>
2019-04-02 16:57:28 +0800

    new 16：56

---
+++ src/com/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.hello;
+
+/**
+ * @ClassName HelloWorld
+ * @Description TODO
+ * Author wangyujiang
+ * @Date 2019/4/2 16:52
+ * @Version 1.0
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World！");
+    }
+}
